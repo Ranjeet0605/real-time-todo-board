918ffae205cc13dcb21e40053b0dff2d99678381
ranjeet0605 <kranjeet0829@gmail.com>
2025-07-05 11:37:48 +0530

    add frontend files all

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Index from "./componants/Index";
+function App() {
+  return (
+    <>
+    <div>
+      helllo rnajeet how are you 
+    </div>
+      <Index />
+      </>
+  );
+}
+
+export default App;
---
+++ frontend/src/componants/Index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Index=()=> {
+  return (
+    <div>
+      this is index page
+    </div>
+  )
+}
+
+export default Index
